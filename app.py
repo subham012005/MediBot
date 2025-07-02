21d29154b55965d3a0948fc284b541fe18c3f39e
SUBHAM <subham2010sh@gmail.com>
2025-07-02 18:25:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,5 +134,5 @@ def ask():
     
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment, fallback 5000
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
+
